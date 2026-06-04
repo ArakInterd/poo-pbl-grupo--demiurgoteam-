ccb49caedefb92051d91b3a090940160064f5863
ArakInterd <interdarak@gmail.com>
2026-06-04 17:06:24 -0300

    Delete src/main/java/crowdfunding directory

--- src/main/java/crowdfunding/domain/entities/StatusDoacao.java
+++
@@ -1,7 +0,0 @@
-package crowdfunding.domain.entities;
-
-public enum StatusDoacao {
-    PENDENTE,
-    CONFIRMADA,
-    ESTORNADA
-}
--- src/main/java/crowdfunding/domain/valueobjects/StatusCampanha.java
+++
@@ -1,9 +0,0 @@
-package crowdfunding.domain.valueobjects;
-
-// estados possíveis de uma campanha
-public enum StatusCampanha {
-    ATIVA,
-    SUCESSO,
-    CANCELADA,
-    EXPIRADA
-}
